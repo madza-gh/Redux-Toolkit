c296fc0f59b2dcfc488ebcbbebef955612b4ff92
madza <mr.ghasemi.1375@gmail.com>
2025-10-11 15:05:20 +0330

    cart and cart items components

---
+++ src/components/CartItems.tsx
@@ -0,0 +1,9 @@
+function CartItems (){
+    return  (
+        <div>
+            <p>محصولی داخل سبد خرید یافت نشد</p>
+        </div>
+    )
+}
+
+export default CartItems
--- src/components/productList.tsx
+++ src/components/productList.tsx
@@ -1,16 +1,15 @@
-import {type ReactNode } from "react";
+import { type ReactNode } from "react";
 
 type ProductListProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
-function ProductList ({children}: ProductListProps ){
-
-    return (
-        <div className="product-list">
-        <div className="product-items">{children}</div>
+function ProductList({ children }: ProductListProps) {
+  return (
+    <div className="product-list">
+      <div className="product-items">{children}</div>
     </div>
-    )
+  );
 }
 
-export default ProductList
+export default ProductList;
