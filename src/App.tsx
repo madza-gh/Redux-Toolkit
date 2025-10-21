29a817121c649051d9f8a3ec2aac63ea74b75ad9
madza <mr.ghasemi.1375@gmail.com>
2025-10-21 14:50:34 +0330

    make Routes for diffrent pagees

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,41 @@ import { Provider } from "react-redux";
 
 import { store } from "./store/store";
 
+import { Route, Routes, Link } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Product from "./components/product";
-import ProductList from "./components/productList";
+
+import ProductList from "./pages/productList";
+import AddProduct from "./pages/AddProduct";
 
 import { productData } from "./data/items";
 
 function app() {
   return (
     <Provider store={store}>
-      <Navbar/>
-      <ProductList>
-        {productData.map((item) => (
-          <div key={item.id}>
-            <Product {...item} />
-          </div>
-        ))}
-      </ProductList>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProductList>
+                {productData.map((item) => (
+                  <div key={item.id}>
+                    <Product {...item} />
+                  </div>
+                ))}
+              </ProductList>
+            }
+          />
+          <Route path="add-product" element = {<AddProduct/>}/>
+          <Route path="*" element ={<h2>404 not found</h2>} />
+        </Routes>
+      </main>
     </Provider>
   );
 }
