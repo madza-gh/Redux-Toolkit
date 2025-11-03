f46e60afba935f973617a4d79e0b240dabb5d555
madza <mr.ghasemi.1375@gmail.com>
2025-11-03 16:05:26 +0330

    modify product slice for apis

--- src/store/productSlice.ts
+++ src/store/productSlice.ts
@@ -1,40 +1,55 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import {getProducts, addProducts,  Product} from '../api/productApi.ts'
 
-type Product={
-    id: string
-    title: string
-    price: number
-    image: string
-}
 
 type ProductState ={
     items: Product[]
+    loading: Boolean
+    error: string | null
 }
 
 const initialState: ProductState ={
-    items: []
+    items: [],
+    loading: false,
+    error: null
 }
+
+export const fetchProducts = createAsyncThunk<Product[]>(
+    "products/fetch",
+    async () => {
+        return await getProducts()  
+    }
+)
+
+export const createProduct = createAsyncThunk<Product, Product>(
+    "products/create",
+    async(productData) =>  {
+        return await addProducts(productData)
+    }
+)
 
 const productSlice = createSlice({
     name: 'product',
     initialState: initialState,
-    reducers:{
-
-        addProduct(state, action: PayloadAction<Product>){
+    reducers:{},
+    extraReducers(builder) {
+        builder
+        .addCase(fetchProducts.pending,(state) =>{
+            state.loading = true
+            state.error = null
+        })
+        .addCase(fetchProducts.fulfilled, (state, action: PayloadAction<Product[]>) =>{
+            state.loading = false
+            state.items = action.payload
+        })
+        .addCase(fetchProducts.rejected, (state, action) =>{
+            state.loading = false
+            state.error = action.error.message || "failed to fetch products"
+        })
+        .addCase(createProduct.fulfilled, (state, action:PayloadAction<Product>) =>{
             state.items.push(action.payload)
-        },
-
-        updateProduct(state, action:PayloadAction<Product>){
-            const itemIndex = state.items.findIndex((item) => item.id === action.payload.id)
-
-            if(itemIndex !== -1){state.items[itemIndex] = action.payload}
-        },
-
-        removeProduct(state, action:PayloadAction<string>){
-            state.items = state.items.filter((item) => item.id !== action.payload)
-        }
-    }
+        })
+    },
 })
 
 export default productSlice.reducer
-export const {addProduct} = productSlice.actions
