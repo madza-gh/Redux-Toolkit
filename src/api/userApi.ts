eacd6f6d7666ec51b88df926fcc780703c36d461
madza <mr.ghasemi.1375@gmail.com>
2025-11-10 14:44:01 +0330

    preparing user api to use

---
+++ src/api/userApi.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_URl = "http://localhost:5000/api/users"
+
+export interface UserType{
+    _id?: string
+    name?: string
+    email: string
+    password: string
+    token?: string
+}
+
+export const login = async (userData: UserType): Promise<UserType> => {
+    const response = await axios.post(`${API_URl}/logIn`,userData)
+
+    return response.data
+}
+
+export const register = async(userData: UserType): Promise<UserType> =>{
+    const response = await axios.post(`${API_URl}/signUp`, userData)
+
+    return response.data
+}
+
+export const userProfile = async (token: string): Promise<UserType> => {
+    const config = {
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.get(`${API_URl}/profile`, config)
+
+    return response.data
+} 
