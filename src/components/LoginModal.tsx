7785f61f32e893d73eae47b17d7ad8643c6f4c16
madza <mr.ghasemi.1375@gmail.com>
2025-11-17 15:16:28 +0330

    connecting Login modal to api (userSlice)

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,14 +1,28 @@
-import { useAppDispatch } from "../store/hooks"
+import { useAppDispatch, useAppSelector } from "../store/hooks"
 import { closeLoginModal } from "../store/uiSlice"
+import { useState } from "react"
+import { loginUser } from "../store/userSlice"
 
 
 function LoginModal(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch()
 
-function handleCloseButton(){
+    const {loading, error, userInfo} = useAppSelector((state) => state.user)
+
+    function handleCloseButton(){
     dispatch(closeLoginModal())
 }
+    function handleOnSubmit (e: React.FormEvent){
+        e.preventDefault()
+
+        dispatch(loginUser({
+            email,
+            password
+        }))
+    }
 
     return(
         <div className="modal-backdrop">
@@ -17,10 +31,10 @@ function handleCloseButton(){
 
                 <h2>ورود</h2>
 
-                <form>
-                    <input type="email" placeholder="ایمیل" />
-                    <input type="password"  placeholder="رمز عبور"/>
-                    <button type="submit">ورود</button>
+                <form onSubmit={handleOnSubmit}>
+                    <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="ایمیل" />
+                    <input onChange={(e) => setPassword(e.target.value)} type="password"  placeholder="رمز عبور"/>
+                    <button type="submit" disabled ={loading}>{loading ?"درحال ورود":"ورود"}</button>
                 </form>
             </div>
         </div>
